3f4d09e696b8a12fc7741f52c134de2a7e939b26
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2017-10-27 18:57:44 +0200

    Add stub of a test

--- test/zucchetti_utils/core_test.clj
+++ test/zucchetti_utils/core_test.clj
@@ -5,3 +5,14 @@
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 0 1))))
+
+
+(deftest test-interval-parsing
+  (testing "happy day, single interval correctly parsed"
+    "   08.31      12.32")
+  (testing "happy day, multiple intervals parsed correctly"
+    "   08.31      12.32      13.16      17.01   ")
+  (testing "happy day, empty interval parsed correctly"
+    "   08.31      8.31")
+  (testing "wrong interval throws an exception"
+    "   08.31      8.12"))
