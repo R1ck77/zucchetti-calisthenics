763ef2676438f8498a3b46c26902c1a6c89ec1a8
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2017-10-28 18:59:37 +0200

    parse-hours and parse-minutes GREEN
    What an ordeal…

--- src/zucchetti_utils/core.clj
+++ src/zucchetti_utils/core.clj
@@ -28,27 +28,39 @@
       value
       :error)))
 
-;;;;; CALISTHENICS VIOLATION
-(defn verify-no-less-than [value min]
-  (verify-value value #(>= % min)))
+(defn- less-or-equal-than-this? [value]
+  (partial <= value))
 
-(defn verify-no-more-than [value max]
-    (verify-value value #(<= % max)))
+(defn- greater-or-equal-than-this? [value]
+  (partial >= value))
 
-(defn convert-time [{s :value
+(defn- verify-my-value [value]
+  (partial verify-value value))
+
+(defn- verify-no-less-than [min value]
+  ((verify-my-value value) (less-or-equal-than-this? min)))
+
+(defn- verify-no-more-than [max value]
+  ((verify-my-value value) (greater-or-equal-than-this? max)))
+
+(defn- verify-no-less-than-this [min]
+  (partial verify-no-less-than min))
+
+(defn- verify-no-more-than-this [max]
+  (partial verify-no-more-than max))
+
+(defn- convert-time [{s :value
                    min-value :min
                    max-value :max}]
-  (verify-no-more-than
-   (verify-no-less-than
-    (parse-integer s) min-value) max-value))
+  ((verify-no-more-than-this max-value)
+   ((verify-no-less-than-this min-value)
+    (parse-integer s))))
 
 (defn parse-hours [s]
   (convert-time {:value s :min 0 :max 24}))
 
 (defn parse-minutes [s]
   (convert-time {:value s :min 0 :max 59}))
-
-;;;;;;;;;;;;;;;;;
 
 (defn parse-time-elements [sh sm]
   )
