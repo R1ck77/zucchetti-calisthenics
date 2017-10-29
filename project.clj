8cc6032eb9a41bb407ca14e73336a2f4fdd36c16
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2017-10-29 18:20:38 +0100

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,5 @@
 (defproject zucchetti-utils "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Calisthenics based on a real-life problem's solution"
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot zucchetti-utils.core
   :target-path "target/%s"
